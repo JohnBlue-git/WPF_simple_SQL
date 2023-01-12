5af0539e0d3db6cbd91897d792254a4bea735cf4
JohnBlue-git <83207141+JohnBlue-git@users.noreply.github.com>
2023-01-12 17:13:17 +0800

    code files

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Collections;
+
+using System.IO;// for Directory.GetCurrentDirectory()
+using System.Data.SqlClient;// for SQL
+//using Microsoft.SqlServer;
+
+// have to reference, but not work
+//using Microsoft.SqlServer.Management.Common;
+//using Microsoft.SqlServer.Management.Smo;
+
+namespace Simple_SQL
+{
+    public class Cus
+    {
+        public string aa; public string bb;
+        public Cus(string a, string b) { aa = a; bb = b; }
+    }
+
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //
+        // sql info
+        //
+        private static SqlConnection connectionString = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=smalldb;Integrated Security=True;Pooling=False");
+
+        //
+        // funtion
+        //
+        private DataTable Select_From_Table(SqlCommand command, string slc)
+        {
+            command.CommandText = slc;
+            command.Connection = connectionString;
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Initial_Function();
+        }
+
+        //
+        // Initial
+        //
+        private void Initial_Function()
+        {
+            connectionString.Open();
+            using (SqlCommand command = new SqlCommand())
+            {
+                try
+                {
+                    FileInfo file = new FileInfo(Directory.GetCurrentDirectory().Substring(0, AppDomain.CurrentDomain.BaseDirectory.Length - 11) + @"\iniDB.sql");
+                    command.CommandText = file.OpenText().ReadToEnd();
+                    command.Connection = connectionString;
+                    command.ExecuteNonQuery();
+
+                    // default: SELECT * From Customer ORDER BY c_id (ASC or DESC)
+                    // [former ... later]
+                    // first order by later ... then order by former
+                    //https://www.fooish.com/sql/order-by.html
+                    CustomerGrid.ItemsSource = Select_From_Table(command, "SELECT * From Customer").DefaultView;
+                    //CustomerGrid.Items.Refresh();
+
+                    AccountGrid.ItemsSource = Select_From_Table(command, "SELECT * From Account").DefaultView;
+                    //AccountGrid.Items.Refresh();
+
+                    // by inner join (the same) 
+                    CrossGrid.ItemsSource = Select_From_Table(command, "SELECT Customer.name, Account.balance FROM Customer INNER JOIN Account ON Customer.account = Account.account").DefaultView;
+                    // by cross join
+                    CrossGrid.ItemsSource = Select_From_Table(command, "SELECT Customer.name, Account.balance FROM Customer CROSS JOIN Account WHERE Customer.account = Account.account").DefaultView;
+                    //CrossGrid.Items.Refresh();
+                }
+                catch (SqlException es)
+                {
+                    // Message
+                    MessageBox.Show(es.ToString());//Console.WriteLine(e.ToString());
+                }
+            }
+            connectionString.Close();
+
+        }
+
+        //
+        // Button
+        //
+        private void Click_Service(object sender, RoutedEventArgs e)
+        {
+            string line;
+            string nm = (sender as Button).Name;
+            if (nm == "Left_Join")
+            {
+                line = "SELECT Customer.name, Customer.account, Account.balance FROM Customer LEFT JOIN Account ON Customer.account = Account.account";
+            }
+            else if (nm == "Right_Join")
+            {
+                // difference of Customer.name, Account.account
+                //           and Customer.name, Customer.account
+                line = "SELECT Customer.name, Account.account, Account.balance FROM Customer RIGHT JOIN Account ON Customer.account = Account.account";
+            }
+            else if (nm == "Full_Join")
+            {
+                line = "SELECT Customer.name, Customer.account, Account.balance FROM Customer FULL JOIN Account ON Customer.account = Account.account";
+            }
+            else if (nm == "Above_Average")
+            {
+                string table = "SELECT Customer.name, Customer.account, Account.balance ";
+                table += "FROM Customer INNER JOIN Account ";
+                table += "ON Customer.account = Account.account ";
+
+                line = "DECLARE @LOCAL_TABLE TABLE(name varchar(30), account int, balance int);";
+                line += "DECLARE @average int;";
+
+                line += "INSERT INTO @LOCAL_TABLE ";
+                line += table;
+
+                line += "SET @average = (SELECT AVG(balance) FROM @LOCAL_TABLE);";
+
+                line += table;
+                line += "AND Account.balance > @average";
+            }
+            else
+            {
+                return;
+            }
+
+            connectionString.Open();
+            using (SqlCommand command = new SqlCommand())
+            {
+                try
+                {
+                    JoinGrid.ItemsSource = Select_From_Table(command, line).DefaultView;
+                    JoinGrid.Items.Refresh();
+                }
+                catch (SqlException es)
+                {
+                    // Message
+                    MessageBox.Show(es.ToString());//Console.WriteLine(e.ToString());
+                }
+            }
+            connectionString.Close();
+        }
+
+        //
+        // Closed
+        //
+        private void Form_Closed(object sender, EventArgs e)
+        {
+
+            connectionString.Open();
+            using (SqlCommand command = new SqlCommand())
+            {
+                try
+                {
+                    command.CommandText = "IF OBJECT_ID(N'Customer') IS NOT NULL BEGIN DROP TABLE Customer END;";
+                    command.CommandText += "IF OBJECT_ID(N'Account') IS NOT NULL BEGIN DROP TABLE Account END";
+                    command.Connection = connectionString;
+                    command.ExecuteNonQuery();
+                }
+                catch (SqlException es)
+                {
+                    // Message
+                    MessageBox.Show(es.ToString());//Console.WriteLine(e.ToString());
+                }
+            }
+            connectionString.Close();
+        }
+        
+    }
+}
---
+++ iniDB.sql
@@ -0,0 +1,37 @@
+﻿
+IF OBJECT_ID(N'Customer') IS NULL
+BEGIN
+  CREATE table Customer(
+    	  --To specify that the "Personid" column should start at value 10 and increment by 5, change it to IDENTITY(10,5).
+          c_id int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+          name varchar(30),
+          account int,
+         );
+       
+  insert into Customer VALUES('A', 103456);
+  --insert into Customer VALUES('B', 303456);--
+  insert into Customer VALUES('C', 203456);
+  insert into Customer VALUES('D', 603456);
+  --insert into Customer VALUES('E', 403456);--
+  insert into Customer VALUES('F', 703456);
+  insert into Customer VALUES('G', 503456);
+  insert into Customer VALUES('H', 803456);
+END
+
+IF OBJECT_ID(N'Account') IS NULL
+BEGIN
+  CREATE table Account(
+          a_id int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+          account int,
+    		  balance int
+         );
+
+  insert into Account VALUES(103456, 10);
+  insert into Account VALUES(303456, 30);
+  --insert into Account VALUES(203456, 20);--
+  insert into Account VALUES(603456, 60);
+  insert into Account VALUES(403456, 40);
+  insert into Account VALUES(703456, 70);
+  insert into Account VALUES(503456, 50);
+  --insert into Account VALUES(803456, 80);--
+END
